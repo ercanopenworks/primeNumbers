fb3b000409b207d01193f062e247f1eecb80f970
Ercan Erdoğan <ercanerdogan@gmail.com>
2019-11-04 13:30:06 +0300

    problem was solved

---
+++ primeNumber/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace primeNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("Enter a count of prime number : ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            int cntr = 0;
+            int sum = 0;
+            bool c = false;
+            for (int i = 2; ; i++)
+            {
+                if (cntr >= number) break;
+                else
+                {
+                    for (int j = 2; j < i; j++)
+                    {
+                        if (i % j == 0)
+                        {
+                            c = true;
+                            break;
+                        }
+                    }
+                    if (c == false)
+                    {
+                        Console.Write(i + " ");
+                        sum += i;
+                        cntr++;
+                    }
+                    c = false;
+                }
+            }
+            Console.WriteLine(@"\n");
+            Console.WriteLine("sum : " + sum.ToString());
+
+            Console.Read();
+        }
+    }
+
+}
+
